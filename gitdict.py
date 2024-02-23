698416c7843cd5b44f0308df476050b7b68d3e5f
Michal Krzyzanowski <mic7email@gmail.com>
2024-02-23 16:35:06 +0000

    Add shopping list dict

---
+++ gitdict.py
@@ -0,0 +1,7 @@
+my_dict = {
+    "bakery": ["bread", "rolls", "donut"],
+    "grocery store": ["carrots", "celery", "arugula"],
+    "sweets shop": ["haribos", "chewing gum"]
+}
+for shop in my_dict:
+    print(f'I am going to the {shop} where I will buy {my_dict[shop]}')
